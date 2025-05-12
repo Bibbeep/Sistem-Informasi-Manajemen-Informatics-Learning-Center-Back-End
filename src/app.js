4dd9206b5916e1d34f8014f4ff877ca761efeb4f
bibbb <muhammadhabibalfarabi290803@gmail.com>
2025-05-13 02:40:39 +0700

    chore: initialize project structure

---
+++ src/app.js
@@ -0,0 +1,22 @@
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config();
+}
+
+const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors');
+
+const app = express();
+
+app.use(
+    cors({
+        origin: process.env.CORS_ORIGIN,
+        credentials: true,
+    }),
+);
+
+app.use(morgan('dev'));
+app.use(express.json({ limit: '10kb' }));
+app.use(express.urlencoded({ extended: true, limit: '10kb' }));
+
+module.exports = { app };
