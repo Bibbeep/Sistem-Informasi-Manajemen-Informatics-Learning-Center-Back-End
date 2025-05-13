9502b5b38b7e255f99e334fd6d749f038a971ab1
bibbb <muhammadhabibalfarabi290803@gmail.com>
2025-05-13 13:09:02 +0700

    chore: added error handler

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ if (process.env.NODE_ENV !== 'production') {
 const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
+const errorHandler = require('./utils/errorHandler');
 
 const app = express();
 
@@ -18,5 +19,7 @@ app.use(
 app.use(morgan('dev'));
 app.use(express.json({ limit: '10kb' }));
 app.use(express.urlencoded({ extended: true, limit: '10kb' }));
+
+app.use(errorHandler);
 
 module.exports = { app };
