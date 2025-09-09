f5adf320fd00b8e7e0bd9ce09b35402c4383eecf
bibbb <muhammadhabibalfarabi290803@gmail.com>
2025-09-09 11:52:37 +0700

    feat: added and passed all integrations test for post /api/v1/auth/register, added user factory, added migrate and truncate scripts, added jest ignore patterns, and also added few npm scripts for testing purpose

--- src/server.js
+++ src/server.js
@@ -1,7 +1,9 @@
 const { app, connectDb } = require('./app');
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, async () => {
+const server = app.listen(PORT, async () => {
     await connectDb();
     console.log(`Server is listening on port ${PORT}`);
 });
+
+module.exports = { server };
