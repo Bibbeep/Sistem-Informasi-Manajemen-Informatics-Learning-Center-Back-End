b123421e5d466b4ef08d749dc60ecb21b418c1f4
bibbb <muhammadhabibalfarabi290803@gmail.com>
2025-09-16 12:53:10 +0700

    docs: add POST /api/v1/auth/register to the swagger documentation

--- src/configs/swagger.js
+++ src/configs/swagger.js
@@ -1,13 +1,24 @@
 const swaggerJSDoc = require('swagger-jsdoc');
 const { version, description } = require('../../package.json');
+const endpointSchemas = require('../validations/schemas/swagger.json');
 
 const options = {
     definition: {
-        openapi: '3.0.0',
+        openapi: '3.1.1',
         info: {
             title: 'Sistem Informasi Manajemen Informatics Learning Center Back End',
             version,
             description,
+        },
+        components: {
+            securitySchemes: {
+                bearerAuth: {
+                    type: 'http',
+                    scheme: 'bearer',
+                    bearerFormat: 'JWT',
+                },
+            },
+            schemas: endpointSchemas,
         },
         servers: [
             {
@@ -16,6 +27,8 @@ const options = {
                     `http://localhost:${process.env.PORT}`,
             },
         ],
+        produces: ['application/json'],
+        schemes: ['https', 'http'],
     },
     apis: ['./src/routes/*.js'],
 };
--- src/routes/auth.route.js
+++ src/routes/auth.route.js
@@ -2,6 +2,50 @@ const router = require('express').Router();
 const AuthController = require('../controllers/auth.controller');
 const { authenticate } = require('../middlewares/auth.middleware');
 
+/**
+ * @openapi
+ * /api/v1/auth/register:
+ *   post:
+ *     summary: Registers a new user account
+ *     tags: [User Authentication]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/RegisterRequestBody'
+ *     responses:
+ *       201:
+ *         description: User registered successfully
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/RegisterSuccessResponse201'
+ *       400:
+ *         description: Invalid request body format
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/RegisterFailedResponse400'
+ *       409:
+ *         description: Email already registered
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/RegisterFailedResponse409'
+ *       429:
+ *         description: Too many requests
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/FailedResponse429'
+ *       500:
+ *         description: Internal server error
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/FailedResponse500'
+ */
 router.post('/register', AuthController.register);
 router.post('/login', AuthController.login);
 router.post('/logout', authenticate, AuthController.logout);
