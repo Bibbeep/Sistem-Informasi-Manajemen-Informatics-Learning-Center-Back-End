e52c695870aa332292294e00e0d3385cd3ef06d0
bibbb <muhammadhabibalfarabi290803@gmail.com>
2025-09-21 21:21:15 +0700

    test(integration): pass all the integration tests for GET /api/v1/users/:userId

--- tests/integrations/user.integration.test.js
+++ tests/integrations/user.integration.test.js
@@ -31,7 +31,7 @@ describe('User Management Integration Tests', () => {
         userArrays = [];
         adminArrays = [];
 
-        for (let i = 0; i < 10; i++) {
+        for (let i = 0; i < 7; i++) {
             userArrays.push(
                 await userFactory({ role: 'User' }, mockUserPassword),
             );
@@ -286,6 +286,150 @@ describe('User Management Integration Tests', () => {
                     ],
                 }),
             );
+        });
+    });
+
+    describe('GET /api/v1/users/:userId', () => {
+        it('should return 200 and fetches user data with admin token', async () => {
+            const response = await request(server)
+                .get('/api/v1/users/1')
+                .set('Authorization', `Bearer ${tokens.validAdmin}`);
+
+            expect(response.status).toBe(200);
+            expect(response.body).toMatchObject({
+                success: true,
+                statusCode: 200,
+                message: 'Successfully retrieved user data.',
+                data: {
+                    user: {
+                        id: 1,
+                        email: expect.any(String),
+                        fullName: expect.any(String),
+                        memberLevel: expect.any(String),
+                        role: expect.any(String),
+                        pictureUrl: expect.anything(),
+                        createdAt: expect.any(String),
+                        updatedAt: expect.any(String),
+                    },
+                },
+                errors: null,
+            });
+        });
+
+        it('should return 200 and fetches user data with user token', async () => {
+            const response = await request(server)
+                .get('/api/v1/users/1')
+                .set('Authorization', `Bearer ${tokens.validUser}`);
+
+            expect(response.status).toBe(200);
+            expect(response.body).toMatchObject({
+                success: true,
+                statusCode: 200,
+                message: 'Successfully retrieved user data.',
+                data: {
+                    user: {
+                        id: 1,
+                        email: expect.any(String),
+                        fullName: expect.any(String),
+                        memberLevel: expect.any(String),
+                        role: expect.any(String),
+                        pictureUrl: expect.anything(),
+                        createdAt: expect.any(String),
+                        updatedAt: expect.any(String),
+                    },
+                },
+                errors: null,
+            });
+        });
+
+        it('should return 400 when path parameter is invalid', async () => {
+            const response = await request(server)
+                .get('/api/v1/users/abc')
+                .set('Authorization', `Bearer ${tokens.validAdmin}`);
+
+            expect(response.status).toBe(400);
+            expect(response.body).toMatchObject({
+                success: false,
+                statusCode: 400,
+                data: null,
+                message: 'Request body validation error.',
+                errors: [
+                    {
+                        message: '"value" must be a number',
+                        context: {
+                            value: 'abc',
+                        },
+                    },
+                ],
+            });
+        });
+
+        it('should return 401 when no authorization provided', async () => {
+            const response = await request(server).get('/api/v1/users/1');
+
+            expect(response.status).toBe(401);
+            expect(response.body).toMatchObject({
+                success: false,
+                statusCode: 401,
+                data: null,
+                message: 'Unauthorized.',
+                errors: [
+                    {
+                        message: 'Invalid or expired token.',
+                        context: {
+                            key: 'request.headers.authorization',
+                            value: null,
+                        },
+                    },
+                ],
+            });
+        });
+
+        it('should return 403 when accessing other user resource', async () => {
+            const response = await request(server)
+                .get('/api/v1/users/4')
+                .set('Authorization', `Bearer ${tokens.validUser}`);
+
+            expect(response.status).toBe(403);
+            expect(response.body).toMatchObject({
+                success: false,
+                statusCode: 403,
+                data: null,
+                message: 'Forbidden.',
+                errors: [
+                    {
+                        message:
+                            'You do not have the necessary permissions to access this resource.',
+                        context: {
+                            key: 'role',
+                            value: 'User',
+                        },
+                    },
+                ],
+            });
+        });
+
+        it('should return 404 when user does not exist', async () => {
+            const response = await request(server)
+                .get('/api/v1/users/404')
+                .set('Authorization', `Bearer ${tokens.validAdmin}`);
+
+            expect(response.status).toBe(404);
+            expect(response.body).toMatchObject({
+                success: false,
+                statusCode: 404,
+                data: null,
+                message: 'Resource not found.',
+                errors: [
+                    {
+                        message: 'User with "userId" does not exist',
+                        context: {
+                            key: 'userId',
+                            value: 404,
+                        },
+                    },
+                ],
+            });
         });
     });
 });
