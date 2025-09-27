5d56e85dba05630c8db52522866483112ab61bab
bibbb <muhammadhabibalfarabi290803@gmail.com>
2025-09-27 23:31:31 +0700

    test(integration): pass all the integration tests for POST /api/v1/feedbacks/:feedbackId/responses

--- tests/integrations/feedback.integration.test.js
+++ tests/integrations/feedback.integration.test.js
@@ -428,4 +428,172 @@ describe('Feedback Management Integration Tests', () => {
             );
         });
     });
+
+    describe('POST /api/v1/feedbacks/:feedbackId/responses', () => {
+        it('should return 201 and creates a new feedback responses', async () => {
+            const mockReqBody = {
+                message:
+                    'Thanks for your feedback John! We are definitely listening to your input and we are working on them!',
+            };
+            const response = await request(server)
+                .post('/api/v1/feedbacks/1/responses')
+                .set('Content-Type', 'application/json')
+                .set('Authorization', `Bearer ${tokens.validAdmin}`)
+                .send(mockReqBody);
+
+            expect(response.status).toBe(201);
+            expect(response.body).toEqual(
+                expect.objectContaining({
+                    success: true,
+                    statusCode: 201,
+                    message: 'Successfully created a feedback response.',
+                    data: {
+                        feedbackResponse: {
+                            id: expect.any(Number),
+                            feedbackId: expect.any(Number),
+                            message: expect.any(String),
+                            adminUserId: expect.any(Number),
+                            updatedAt: expect.any(String),
+                            createdAt: expect.any(String),
+                        },
+                    },
+                    errors: null,
+                }),
+            );
+        });
+
+        it('should return 400 with invalid request body', async () => {
+            const mockReqBody = {
+                message: 123,
+            };
+            const response = await request(server)
+                .post('/api/v1/feedbacks/1/responses')
+                .set('Content-Type', 'application/json')
+                .set('Authorization', `Bearer ${tokens.validAdmin}`)
+                .send(mockReqBody);
+
+            expect(response.status).toBe(400);
+            expect(response.body).toEqual(
+                expect.objectContaining({
+                    success: false,
+                    statusCode: 400,
+                    data: null,
+                    message: 'Validation error.',
+                    errors: expect.any(Array),
+                }),
+            );
+        });
+
+        it('should return 401 with invalid access token', async () => {
+            const mockReqBody = {
+                message:
+                    'Thanks for your feedback John! We are definitely listening to your input and we are working on them!',
+            };
+            const response = await request(server)
+                .post('/api/v1/feedbacks/1/responses')
+                .set('Content-Type', 'application/json')
+                .send(mockReqBody);
+
+            expect(response.status).toBe(401);
+            expect(response.body).toEqual(
+                expect.objectContaining({
+                    success: false,
+                    statusCode: 401,
+                    data: null,
+                    message: 'Unauthorized.',
+                    errors: [
+                        {
+                            message: 'Invalid or expired token.',
+                            context: {
+                                key: 'request.headers.authorization',
+                                value: null,
+                            },
+                        },
+                    ],
+                }),
+            );
+        });
+
+        it('should return 403 with user access token', async () => {
+            const mockReqBody = {
+                message:
+                    'Thanks for your feedback John! We are definitely listening to your input and we are working on them!',
+            };
+            const response = await request(server)
+                .post('/api/v1/feedbacks/1/responses')
+                .set('Content-Type', 'application/json')
+                .set('Authorization', `Bearer ${tokens.validUser}`)
+                .send(mockReqBody);
+
+            expect(response.status).toBe(403);
+            expect(response.body).toEqual(
+                expect.objectContaining({
+                    success: false,
+                    statusCode: 403,
+                    data: null,
+                    message: 'Forbidden.',
+                    errors: [
+                        {
+                            message:
+                                'You do not have the necessary permissions to access this resource.',
+                            context: {
+                                key: 'role',
+                                value: 'User',
+                            },
+                        },
+                    ],
+                }),
+            );
+        });
+
+        it('should return 404 when feedback does not exist', async () => {
+            const mockReqBody = {
+                message:
+                    'Thanks for your feedback John! We are definitely listening to your input and we are working on them!',
+            };
+            const response = await request(server)
+                .post('/api/v1/feedbacks/404/responses')
+                .set('Content-Type', 'application/json')
+                .set('Authorization', `Bearer ${tokens.validAdmin}`)
+                .send(mockReqBody);
+
+            expect(response.status).toBe(404);
+            expect(response.body).toEqual(
+                expect.objectContaining({
+                    success: false,
+                    statusCode: 404,
+                    data: null,
+                    message: 'Resource not found.',
+                    errors: [
+                        {
+                            message:
+                                'Feedback with "feedbackId" does not exist',
+                            context: {
+                                key: 'feedbackId',
+                                value: 404,
+                            },
+                        },
+                    ],
+                }),
+            );
+        });
+
+        it('should return 415 with invalid Content-Type header', async () => {
+            const response = await request(server)
+                .post('/api/v1/feedbacks/1/responses')
+                .set('Content-Type', 'multipart/form-data')
+                .set('Authorization', `Bearer ${tokens.validAdmin}`);
+
+            expect(response.status).toBe(415);
+            expect(response.body).toEqual(
+                expect.objectContaining({
+                    success: false,
+                    statusCode: 415,
+                    data: null,
+                    message: 'Unsupported Media Type.',
+                    errors: expect.any(Array),
+                }),
+            );
+        });
+    });
 });
