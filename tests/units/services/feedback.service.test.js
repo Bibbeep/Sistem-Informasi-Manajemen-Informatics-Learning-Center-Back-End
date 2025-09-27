7ecb23f597e058cb819407645cda8aa968b8ade8
bibbb <muhammadhabibalfarabi290803@gmail.com>
2025-09-27 19:45:53 +0700

    test(unit): pass all unit tests for feedback getOne service

--- tests/units/services/feedback.service.test.js
+++ tests/units/services/feedback.service.test.js
@@ -2,6 +2,7 @@
 jest.mock('../../../src/db/models');
 const FeedbackService = require('../../../src/services/feedback.service');
 const { Feedback, FeedbackResponse } = require('../../../src/db/models');
+const HTTPError = require('../../../src/utils/httpError');
 
 describe('Feedback Service Unit Tests', () => {
     afterEach(() => {
@@ -290,6 +291,78 @@ describe('Feedback Service Unit Tests', () => {
                 order: [['id', 'ASC']],
             });
             expect(returnValue).toStrictEqual(mockReturnValue);
+        });
+    });
+
+    describe('getOne Tests', () => {
+        it('should return feedback data', async () => {
+            const mockFeedbackId = 1;
+            const mockFeedbackData = {
+                id: 1,
+                email: 'Salsabila.Nasyiah@yahoo.co.id',
+                fullName: 'Zalindra Lestari',
+                message: 'Contigo possimus dignissimos tamdiu.',
+                createdAt: '2025-03-12T03:13:10.990Z',
+                updatedAt: '2025-07-19T16:11:02.483Z',
+                responses: [
+                    {
+                        id: 1,
+                        adminUserId: 2,
+                        message:
+                            'Considero tenus acidus acquiro demitto illo corporis degero. Optio tumultus vulgaris valde creptio vilicus fuga tertius ubi.',
+                        createdAt: '2025-03-20T20:45:47.619Z',
+                        updatedAt: '2025-05-18T11:04:15.275Z',
+                    },
+                ],
+            };
+
+            Feedback.findByPk.mockResolvedValue(mockFeedbackData);
+
+            const returnValue = await FeedbackService.getOne(mockFeedbackId);
+
+            expect(Feedback.findByPk).toHaveBeenCalledWith(mockFeedbackId, {
+                include: [
+                    {
+                        model: FeedbackResponse,
+                        as: 'responses',
+                        attributes: {
+                            exclude: ['feedbackId'],
+                        },
+                    },
+                ],
+            });
+            expect(returnValue).toStrictEqual(mockFeedbackData);
+        });
+
+        it('should throw 404 error when feedback does not exist', async () => {
+            const mockFeedbackId = 404;
+
+            Feedback.findByPk.mockResolvedValue(null);
+
+            await expect(
+                FeedbackService.getOne(mockFeedbackId),
+            ).rejects.toThrow(
+                new HTTPError(404, 'Resource not found.', [
+                    {
+                        message: 'Feedback with "feedbackId" does not exist',
+                        context: {
+                            key: 'feedbackId',
+                            value: mockFeedbackId,
+                        },
+                    },
+                ]),
+            );
+            expect(Feedback.findByPk).toHaveBeenCalledWith(mockFeedbackId, {
+                include: [
+                    {
+                        model: FeedbackResponse,
+                        as: 'responses',
+                        attributes: {
+                            exclude: ['feedbackId'],
+                        },
+                    },
+                ],
+            });
         });
     });
 });
