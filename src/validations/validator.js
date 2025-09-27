3218c88858b40e5b38587049163a4bb594905ea6
bibbb <muhammadhabibalfarabi290803@gmail.com>
2025-09-27 14:12:40 +0700

    refactor: move userUpdate validation schema out to user schema and uniqueIdentifier out to global validator module

--- src/validations/validator.js
+++ src/validations/validator.js
@@ -1,14 +1,16 @@
+const Joi = require('joi');
 const {
     register,
     login,
     tokenPayload,
     forgotPassword,
     resetPassword,
-    uniqueIdentifier,
-    userUpdate,
 } = require('./schemas/auth');
-const { userQueryParam } = require('./schemas/user');
+const { userQueryParam, userUpdate } = require('./schemas/user');
 const { feedbackQueryParam } = require('./schemas/feedback');
+
+// Any auto-increment integer id
+const uniqueIdentifier = Joi.number().integer().positive().required();
 
 const validator = (schema) => {
     return (payload) => {
