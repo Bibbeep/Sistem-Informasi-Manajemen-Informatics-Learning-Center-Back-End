9c55cb6993e666bbfd9edc2c2a475f0571f087cd
bibbb <muhammadhabibalfarabi290803@gmail.com>
2025-10-04 00:39:58 +0700

    test(unit): pass all unit tests for enrollment getById controller

--- tests/units/controllers/enrollment.controller.test.js
+++ tests/units/controllers/enrollment.controller.test.js
@@ -1,7 +1,10 @@
 /* eslint-disable no-undef */
 jest.mock('../../../src/validations/validator');
 jest.mock('../../../src/services/enrollment.service');
-const { getAll } = require('../../../src/controllers/enrollment.controller');
+const {
+    getAll,
+    getById,
+} = require('../../../src/controllers/enrollment.controller');
 const EnrollmentService = require('../../../src/services/enrollment.service');
 const {
     validateEnrollmentQuery,
@@ -149,6 +152,44 @@ describe('Enrollment Controller Unit Tests', () => {
 
             expect(validateEnrollmentQuery).toHaveBeenCalledWith(req.query);
             expect(EnrollmentService.getMany).toHaveBeenCalledWith(mockValue);
+            expect(next).toHaveBeenCalledWith(mockError);
+            expect(res.status).not.toHaveBeenCalled();
+            expect(res.json).not.toHaveBeenCalled();
+        });
+    });
+
+    describe('getById Tests', () => {
+        it('should sends 200 on success and does not call next', async () => {
+            req.params = { enrollmentId: '1' };
+            const mockEnrollment = {
+                id: 1,
+            };
+            EnrollmentService.getOne.mockResolvedValue(mockEnrollment);
+
+            await getById(req, res, next);
+
+            expect(EnrollmentService.getOne).toHaveBeenCalledWith(1);
+            expect(next).not.toHaveBeenCalled();
+            expect(res.status).toHaveBeenCalledWith(200);
+            expect(res.json).toHaveBeenCalledWith({
+                success: true,
+                statusCode: 200,
+                message: 'Successfully retrieved program enrollment details.',
+                data: {
+                    enrollment: mockEnrollment,
+                },
+                errors: null,
+            });
+        });
+
+        it('should forwards service errors to next', async () => {
+            req.params = { enrollmentId: '1' };
+            const mockError = new Error('BOOM');
+            EnrollmentService.getOne.mockRejectedValue(mockError);
+
+            await getById(req, res, next);
+
+            expect(EnrollmentService.getOne).toHaveBeenCalledWith(1);
             expect(next).toHaveBeenCalledWith(mockError);
             expect(res.status).not.toHaveBeenCalled();
             expect(res.json).not.toHaveBeenCalled();
