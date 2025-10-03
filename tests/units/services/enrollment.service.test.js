ceda677b1d7d54f6420cfc1aad73c61316c16e9f
bibbb <muhammadhabibalfarabi290803@gmail.com>
2025-10-04 00:29:58 +0700

    test(unit): pass all unit tests for enrollment getOne service

--- tests/units/services/enrollment.service.test.js
+++ tests/units/services/enrollment.service.test.js
@@ -2,6 +2,7 @@
 jest.mock('../../../src/db/models');
 const EnrollmentService = require('../../../src/services/enrollment.service');
 const { Enrollment } = require('../../../src/db/models');
+const HTTPError = require('../../../src/utils/httpError');
 
 describe('Enrollment Service Unit Tests', () => {
     afterEach(() => {
@@ -255,6 +256,106 @@ describe('Enrollment Service Unit Tests', () => {
                 expect.any(Object),
             );
             expect(result).toEqual(mockResult);
+        });
+    });
+
+    describe('getOne Tests', () => {
+        it('should return enrollment data for Course program', async () => {
+            const mockEnrollmentId = 1;
+            const mockEnrollment = {
+                id: 1,
+                userId: 1,
+                programId: 1,
+                progressPercentage: 100.0,
+                status: 'Completed',
+                completedAt: 'NOW',
+                createdAt: 'NOW',
+                updatedAt: 'NOW',
+                program: {
+                    title: 'title',
+                    type: 'Course',
+                    thumbnailUrl: 'https://thumbnail.com/url',
+                },
+                completedModules: [
+                    {
+                        courseModuleId: 1,
+                        completedAt: 'NOW',
+                    },
+                    {
+                        courseModuleId: 1,
+                        completedAt: 'NOW',
+                    },
+                    {
+                        courseModuleId: 1,
+                        completedAt: 'NOW',
+                    },
+                ],
+            };
+            Enrollment.findByPk.mockResolvedValue(mockEnrollment);
+
+            const result = await EnrollmentService.getOne(mockEnrollmentId);
+
+            expect(Enrollment.findByPk).toHaveBeenCalledWith(
+                mockEnrollmentId,
+                expect.any(Object),
+            );
+            expect(result).toHaveProperty(
+                'completedModules',
+                expect.any(Array),
+            );
+        });
+
+        it('should return enrollment data for non-Course program', async () => {
+            const mockEnrollmentId = 1;
+            const mockEnrollment = {
+                id: 1,
+                userId: 1,
+                programId: 1,
+                progressPercentage: 100.0,
+                status: 'Completed',
+                completedAt: 'NOW',
+                createdAt: 'NOW',
+                updatedAt: 'NOW',
+                program: {
+                    title: 'title',
+                    type: 'Seminar',
+                    thumbnailUrl: 'https://thumbnail.com/url',
+                },
+            };
+            Enrollment.findByPk.mockResolvedValue(mockEnrollment);
+
+            const result = await EnrollmentService.getOne(mockEnrollmentId);
+
+            expect(Enrollment.findByPk).toHaveBeenCalledWith(
+                mockEnrollmentId,
+                expect.any(Object),
+            );
+            expect(result).not.toHaveProperty(
+                'completedModules',
+                expect.any(Array),
+            );
+        });
+
+        it('should throw 404 error when enrollment does not exist', async () => {
+            const mockEnrollmentId = 404;
+            const mockError = new HTTPError(404, 'Resource not found.', [
+                {
+                    message: 'Enrollment with "enrollmentId" does not exist',
+                    context: {
+                        key: 'enrollmentId',
+                        value: mockEnrollmentId,
+                    },
+                },
+            ]);
+            Enrollment.findByPk.mockResolvedValue(null);
+
+            await expect(
+                EnrollmentService.getOne(mockEnrollmentId),
+            ).rejects.toThrow(mockError);
+            expect(Enrollment.findByPk).toHaveBeenCalledWith(
+                mockEnrollmentId,
+                expect.any(Object),
+            );
         });
     });
 });
