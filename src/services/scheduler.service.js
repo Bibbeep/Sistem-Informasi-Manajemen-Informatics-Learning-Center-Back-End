f9678f5ecbe8a74b45d9ac9ca2e8f3ccad974dbe
bibbb <muhammadhabibalfarabi290803@gmail.com>
2025-10-08 21:25:34 +0700

    refactor: add Expired enrollment into the seeder

---
+++ src/services/scheduler.service.js
@@ -0,0 +1,12 @@
+const cron = require('node-cron');
+
+class SchedulerService {
+    /**
+     * Start cron job to check whether invoice's paymentDueDatetime is past expiration and change its status
+     * @todo Will do :p
+     * @static
+     */
+    static start() {
+        //
+    }
+}
