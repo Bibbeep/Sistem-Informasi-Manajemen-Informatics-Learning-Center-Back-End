7d4716f35497733ed776615584f32c019ac99235
bibbb <muhammadhabibalfarabi290803@gmail.com>
2025-10-08 22:03:38 +0700

    feat: add SchedulerService for cron job to check invoice payment due

--- src/server.js
+++ src/server.js
@@ -4,6 +4,7 @@ const { connectDb } = require('./configs/database');
 const { connectRedis } = require('./configs/redis');
 const { connectNodemailer } = require('./configs/nodemailer');
 const { connectS3 } = require('./configs/s3');
+const SchedulerService = require('./services/scheduler.service');
 /* istanbul ignore next */
 const PORT = process.env.PORT || 3000;
 
@@ -12,6 +13,9 @@ const server = app.listen(PORT, async () => {
     await connectRedis();
     await connectNodemailer();
     await connectS3();
+
+    SchedulerService.start();
+
     console.log(chalk.inverse.bold(`Server is listening on port ${PORT}`));
 });
 
--- src/services/scheduler.service.js
+++ src/services/scheduler.service.js
@@ -1,12 +1,70 @@
 const cron = require('node-cron');
+const chalk = require('chalk');
+const { Op } = require('sequelize');
+const { Invoice, Enrollment, sequelize } = require('../db/models');
 
 class SchedulerService {
     /**
-     * Start cron job to check whether invoice's paymentDueDatetime is past expiration and change its status
-     * @todo Will do :p
+     * Start cron job to check whether invoice's paymentDueDatetime is past expiration and change its status for every minute (* * * * *)
      * @static
      */
     static start() {
-        //
+        console.log(chalk.blue('[Cron Job]'), 'Job is starting');
+
+        cron.schedule('* * * * *', async () => {
+            try {
+                await sequelize.transaction(async (t) => {
+                    const expiredInvoices = await Invoice.findAll(
+                        {
+                            where: {
+                                status: 'Unverified',
+                                paymentDueDatetime: {
+                                    [Op.lte]: new Date(),
+                                },
+                            },
+                        },
+                        {
+                            transaction: t,
+                        },
+                    );
+
+                    if (expiredInvoices.length > 0) {
+                        const enrollmentIds = expiredInvoices.map((invoice) => {
+                            return invoice.enrollmentId;
+                        });
+
+                        await Invoice.update(
+                            {
+                                status: 'Expired',
+                            },
+                            {
+                                where: {
+                                    id: expiredInvoices.map((invoice) => {
+                                        return invoice.id;
+                                    }),
+                                },
+                                transaction: t,
+                            },
+                        );
+
+                        await Enrollment.update(
+                            {
+                                status: 'Expired',
+                            },
+                            {
+                                where: {
+                                    id: enrollmentIds,
+                                },
+                                transaction: t,
+                            },
+                        );
+                    }
+                });
+            } catch (err) {
+                console.error('[Cron Job] Error:', err);
+            }
+        });
     }
 }
+
+module.exports = SchedulerService;
