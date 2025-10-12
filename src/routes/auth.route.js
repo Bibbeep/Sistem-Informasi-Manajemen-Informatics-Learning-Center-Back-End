49d26eb3eaca7b4a0221259b28795d03026255d2
bibbb <muhammadhabibalfarabi290803@gmail.com>
2025-10-13 00:03:28 +0700

    refactor: add asyncHandler to wrap controllers

--- src/routes/auth.route.js
+++ src/routes/auth.route.js
@@ -2,19 +2,24 @@ const router = require('express').Router();
 const AuthController = require('../controllers/auth.controller');
 const { authenticate } = require('../middlewares/auth.middleware');
 const { requireJsonContent } = require('../middlewares/contentType.middleware');
+const asyncHandler = require('../utils/asyncHandler');
 
-router.post('/register', requireJsonContent, AuthController.register);
-router.post('/login', requireJsonContent, AuthController.login);
-router.post('/logout', authenticate, AuthController.logout);
+router.post(
+    '/register',
+    requireJsonContent,
+    asyncHandler(AuthController.register),
+);
+router.post('/login', requireJsonContent, asyncHandler(AuthController.login));
+router.post('/logout', authenticate, asyncHandler(AuthController.logout));
 router.post(
     '/forgot-password',
     requireJsonContent,
-    AuthController.forgotPassword,
+    asyncHandler(AuthController.forgotPassword),
 );
 router.post(
     '/reset-password',
     requireJsonContent,
-    AuthController.resetPassword,
+    asyncHandler(AuthController.resetPassword),
 );
 
 module.exports = router;
