0d90998c1fe4c6fbc6bb854b6e2e49fbb0e812f0
bibbb <muhammadhabibalfarabi290803@gmail.com>
2025-10-13 01:37:03 +0700

    fix: add only apply strict rate limiter on production env

--- src/configs/rateLimiter.js
+++ src/configs/rateLimiter.js
@@ -1,6 +1,9 @@
 module.exports = {
     windowMs: process.env.RATE_LIMITER_WINDOW_SEC * 1000 || 60 * 1000,
-    max: process.env.RATE_LIMITER_MAX_REQ || 100,
+    max:
+        process.env.NODE_ENV !== 'production'
+            ? 600
+            : process.env.RATE_LIMITER_MAX_REQ || 100,
     // eslint-disable-next-line no-unused-vars
     handler: (req, res, next) => {
         return res.status(429).json({
