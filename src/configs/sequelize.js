e8e1310cc975b0c788719153a9045fd2d66f9d86
bibbb <muhammadhabibalfarabi290803@gmail.com>
2025-10-15 00:36:47 +0700

    chore: add render.yaml and change some environament variables

--- src/configs/sequelize.js
+++ src/configs/sequelize.js
@@ -4,29 +4,17 @@ if (process.env.NODE_ENV !== 'production') {
 
 module.exports = {
     development: {
-        username: process.env.POSTGRES_USER,
-        password: process.env.POSTGRES_PASSWORD,
-        database: process.env.POSTGRES_DB,
-        host: process.env.POSTGRES_HOST,
-        port: process.env.POSTGRES_PORT,
+        use_env_variable: 'DATABASE_URL',
         dialect: 'postgres',
         logging: false,
     },
     test: {
-        username: process.env.POSTGRES_USER || 'postgres',
-        password: process.env.POSTGRES_PASSWORD || null,
-        database: 'sim_ilc_test',
-        host: '127.0.0.1',
-        port: '5432',
+        use_env_variable: 'DATABASE_URL',
         dialect: 'postgres',
         logging: false,
     },
     production: {
-        username: process.env.POSTGRES_USER,
-        password: process.env.POSTGRES_PASSWORD,
-        database: process.env.POSTGRES_DB,
-        host: process.env.POSTGRES_HOST,
-        port: process.env.POSTGRES_PORT,
+        use_env_variable: 'DATABASE_URL',
         dialect: 'postgres',
         dialectOptions: {
             ssl: {
